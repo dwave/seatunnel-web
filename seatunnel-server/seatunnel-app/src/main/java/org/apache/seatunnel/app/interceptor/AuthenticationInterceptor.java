e178298a0ff6b1989390b19c9e3bcc6b41d52e86
dwave <2606529150@qq.com>
2025-01-20 10:11:15 +0800

    数据采集修改ok

--- seatunnel-server/seatunnel-app/src/main/java/org/apache/seatunnel/app/interceptor/AuthenticationInterceptor.java
+++ seatunnel-server/seatunnel-app/src/main/java/org/apache/seatunnel/app/interceptor/AuthenticationInterceptor.java
@@ -20,12 +20,8 @@ package org.apache.seatunnel.app.interceptor;
 import org.apache.seatunnel.app.common.Constants;
 import org.apache.seatunnel.app.dal.dao.IUserDao;
 import org.apache.seatunnel.app.dal.entity.User;
-import org.apache.seatunnel.app.dal.entity.UserLoginLog;
 import org.apache.seatunnel.app.security.JwtUtils;
 
-import org.apache.commons.lang3.StringUtils;
-
-import org.eclipse.jetty.http.HttpStatus;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -35,13 +31,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.util.Map;
-import java.util.Objects;
-
-import static io.jsonwebtoken.Claims.EXPIRATION;
 import static org.apache.seatunnel.server.common.Constants.OPTIONS;
-import static org.apache.seatunnel.server.common.Constants.TOKEN;
-import static org.apache.seatunnel.server.common.Constants.USER_ID;
 
 @Slf4j
 public class AuthenticationInterceptor implements HandlerInterceptor {
@@ -65,41 +55,41 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
         }
 
         long currentTimestamp = System.currentTimeMillis();
-//        final String token = request.getHeader(TOKEN);
-//        if (StringUtils.isBlank(token)) {
-//            log.info("user does not exist");
-////            response.setStatus(HttpStatus.UNAUTHORIZED_401);
-//            return false;
-//        }
-//        final Map<String, Object> map = jwtUtils.parseToken(token);
-//        final Integer userId = (Integer) map.get(USER_ID);
-//        if (Objects.isNull(userId)) {
-//            log.info("userId does not exist");
-////            response.setStatus(HttpStatus.UNAUTHORIZED_401);
-//            return false;
-//        }
-//        final UserLoginLog userLoginLog = userDaoImpl.getLastLoginLog(userId);
-//        if (Objects.isNull(userLoginLog) || !userLoginLog.getTokenStatus()) {
-//            log.info("userLoginLog does not exist");
-////            response.setStatus(HttpStatus.UNAUTHORIZED_401);
-//            return false;
-//        }
-//
-//        final Integer expireDate = (Integer) map.get(EXPIRATION);
-//        if (Objects.isNull(expireDate) || currentTimestamp - (long) expireDate * 1000 > 0) {
-//            log.info("user token has expired");
-//            response.setStatus(HttpStatus.UNAUTHORIZED_401);
-//            return false;
-//        }
+        //        final String token = request.getHeader(TOKEN);
+        //        if (StringUtils.isBlank(token)) {
+        //            log.info("user does not exist");
+        ////            response.setStatus(HttpStatus.UNAUTHORIZED_401);
+        //            return false;
+        //        }
+        //        final Map<String, Object> map = jwtUtils.parseToken(token);
+        //        final Integer userId = (Integer) map.get(USER_ID);
+        //        if (Objects.isNull(userId)) {
+        //            log.info("userId does not exist");
+        ////            response.setStatus(HttpStatus.UNAUTHORIZED_401);
+        //            return false;
+        //        }
+        //        final UserLoginLog userLoginLog = userDaoImpl.getLastLoginLog(userId);
+        //        if (Objects.isNull(userLoginLog) || !userLoginLog.getTokenStatus()) {
+        //            log.info("userLoginLog does not exist");
+        ////            response.setStatus(HttpStatus.UNAUTHORIZED_401);
+        //            return false;
+        //        }
+        //
+        //        final Integer expireDate = (Integer) map.get(EXPIRATION);
+        //        if (Objects.isNull(expireDate) || currentTimestamp - (long) expireDate * 1000 > 0)
+        // {
+        //            log.info("user token has expired");
+        //            response.setStatus(HttpStatus.UNAUTHORIZED_401);
+        //            return false;
+        //        }
 
-
-//        map.forEach(request::setAttribute);
+        //        map.forEach(request::setAttribute);
         Integer userId = 2;
         User user = new User();
         user.setUsername("admin");
         user.setId(userId);
-//        user.setUsername((String) map.get("name"));
-//        user.setId((Integer) map.get("id"));
+        //        user.setUsername((String) map.get("name"));
+        //        user.setId((Integer) map.get("id"));
         //        user.setStatus((Byte) map.get("status"));
         //        user.setType((Byte) map.get("type"));
         request.setAttribute(Constants.SESSION_USER, user);
